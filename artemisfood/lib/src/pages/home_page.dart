3344d9074cdb01bb73b81d478bd6ffd4f816448c
Roger <RogerAlmeydaRamos@outlook.com>
2020-07-21 11:43:52 -0500

    navegationbar

--- artemisfood/lib/src/pages/home_page.dart
+++ artemisfood/lib/src/pages/home_page.dart
@@ -1,15 +1,60 @@
 import 'package:artemisfood/src/components/background_home.dart';
-import 'package:artemisfood/src/static/const.dart';
+import 'package:artemisfood/src/pages/order.dart';
+import 'package:artemisfood/src/pages/profile.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  List<Widget> _widgetOptions = <Widget>[
+    BackgroundHome(),
+    Order(),
+    Profile(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BackgroundHome(),
+      bottomNavigationBar: ClipRRect(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(17.0), topRight: Radius.circular(17.0)),
+          child: BottomNavigationBar(
+              currentIndex: _selectedIndex,
+              onTap: _onItemTapped,
+              backgroundColor: Colors.grey[200],
+              iconSize: 30.0,
+              unselectedItemColor: Colors.black,
+              items: <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  title: Container(),
+                  backgroundColor: Colors.black,
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.receipt),
+                  title: Container(),
+                  backgroundColor: Colors.black,
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.person),
+                  title: Container(),
+                  backgroundColor: Colors.black,
+                ),
+              ])),
+      body: _widgetOptions.elementAt(_selectedIndex),
     );
   }
 }
-
