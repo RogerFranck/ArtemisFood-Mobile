237dd53e9e8299aec411a1a3856aadfd14889653
jirenuki-69 <mfuentesmodelo@gmail.com>
2020-07-20 20:25:31 -0500

    listview de los favoritos

--- artemisfood/lib/src/components/body_home.dart
+++ artemisfood/lib/src/components/body_home.dart
@@ -41,30 +41,46 @@ class BodyHome extends StatelessWidget {
           SizedBox( height: 20.0 ),
           SearchField(),
           SizedBox( height: 20.0 ),
-          Container(
-            width: size.width * 0.88,
-            height: 300.0,
-            child: Column(
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Row(
               children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.favorite,
-                      color: Colors.white,
-                    ),
-                    SizedBox( width: 5.0, ),
-                    Text(
-                      'Favorites',
-                      style: TextStyle( fontWeight: FontWeight.bold, color: Colors.white, fontSize: 22.0 ),
-                    ),
-                  ],
+                Icon(
+                  Icons.favorite,
+                  color: Colors.white,
                 ),
-                SizedBox( height: 20.0, ),
-                ListaFavoritos(),
+                SizedBox( width: 5.0, ),
+                Text(
+                  'Favorites',
+                  style: TextStyle( fontWeight: FontWeight.bold, color: Colors.white, fontSize: 22.0 ),
+                ),
               ],
             ),
           ),
+          ListViewFavoritos(),
         ],
+      ),
+    );
+  }
+}
+
+class ListViewFavoritos extends StatelessWidget {
+  const ListViewFavoritos({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Container(
+      width: size.width,
+      height: 250,
+      child: ListView.separated(
+        scrollDirection: Axis.horizontal,
+        separatorBuilder: ( context, index ) => Container( width: 15.0 ,),
+        itemCount: 5,
+        itemBuilder: ( context, index ) {
+          return ListaFavoritos();
+        },
       ),
     );
   }
@@ -80,56 +96,59 @@ class ListaFavoritos extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    return Stack(
-      alignment: Alignment.center,
-      children: <Widget>[
-        ClipRRect(
-          borderRadius: BorderRadius.circular(15.0),
-          child: Image(
-            image: NetworkImage('https://s2.best-wallpaper.net/wallpaper/2880x1800/1808/Hamburger-meat-French-fries-fast-food_2880x1800.jpg'),
+    return Container(
+      width: size.width * 0.88,
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(15.0),
+            child: Image(
+              image: NetworkImage('https://s2.best-wallpaper.net/wallpaper/2880x1800/1808/Hamburger-meat-French-fries-fast-food_2880x1800.jpg'),
+            ),
           ),
-        ),
-        Positioned(
-          bottom: 20,
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(10.0),
-            child: Container(
-              width: size.width * 0.75,
-              height: 60.0,
-              color: Colors.white,
-              child: Padding(
-                padding: const EdgeInsets.all(6.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          'Cheseseburguer',
-                          style: primaryTitle,
-                        ),
-                        Divider(height: 7.0,),
-                        Text(
-                          'Tomate, cebolla, pepinillos, etc.',
-                          style: primarySubtitle,
-                        ),
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(right: 8.0),
-                      child: Text(
-                        '100 MXN',
-                        style: TextStyle( fontWeight: FontWeight.bold, fontSize: 18.0 ),
+          Positioned(
+            bottom: 20,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10.0),
+              child: Container(
+                width: size.width * 0.75,
+                height: 60.0,
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.all(6.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Cheseseburguer',
+                            style: primaryTitle,
+                          ),
+                          Divider(height: 7.0,),
+                          Text(
+                            'Tomate, cebolla, pepinillos, etc.',
+                            style: primarySubtitle,
+                          ),
+                        ],
                       ),
-                    ),
-                  ],
+                      Padding(
+                        padding: const EdgeInsets.only(right: 8.0),
+                        child: Text(
+                          '100 MXN',
+                          style: TextStyle( fontWeight: FontWeight.bold, fontSize: 18.0 ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
