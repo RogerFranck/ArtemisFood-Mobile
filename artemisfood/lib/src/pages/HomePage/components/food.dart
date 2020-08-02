99eb7e7aadecbabc408d9361aaac1befb55334a9
RogerFranck <rogereschido@gmail.com>
2020-08-01 21:20:35 -0500

    cosas

--- artemisfood/lib/src/pages/HomePage/components/food.dart
+++ artemisfood/lib/src/pages/HomePage/components/food.dart
@@ -78,96 +78,99 @@ class FoodItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(bottom: 20),
-      child: Container(
-        height: 250.0,
-        child: FractionallySizedBox(
-          widthFactor: 0.9,
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20.0),
-              boxShadow: <BoxShadow>[
-                BoxShadow(
-                  color: Colors.black26,
-                  blurRadius: 10.0,
-                  spreadRadius: 1.0,
-                  offset: Offset(2.0, 10.0)
-                ),
-              ],
-            ),
-            child: Column(
-              children: <Widget>[
-                FractionallySizedBox(
-                  widthFactor: 1,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.vertical(top: Radius.circular(20.0),),
-                    child: Container(
-                      height: 175.0,
-                      child: Image( 
-                        image: NetworkImage(imagen),
-                        fit: BoxFit.cover,
+    return FlatButton(
+      onPressed: (){},
+      child: Padding(
+        padding: EdgeInsets.only(bottom: 20),
+        child: Container(
+          height: 250.0,
+          child: FractionallySizedBox(
+            widthFactor: 0.9,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20.0),
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 10.0,
+                    spreadRadius: 1.0,
+                    offset: Offset(2.0, 5.0)
+                  ),
+                ],
+              ),
+              child: Column(
+                children: <Widget>[
+                  FractionallySizedBox(
+                    widthFactor: 1,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.vertical(top: Radius.circular(20.0),),
+                      child: Container(
+                        height: 175.0,
+                        child: Image( 
+                          image: NetworkImage(imagen),
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Flexible(
-                        flex: 7,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              nombre,
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18.0
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Flexible(
+                          flex: 7,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                nombre,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18.0
+                                ),
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
                               ),
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                            SizedBox(
-                              height: 3.0,
-                            ),
-                            Text(
-                              'Con pepinillos, salsa de tomate, vegetales, etc.',
-                              style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 10.0
+                              SizedBox(
+                                height: 3.0,
                               ),
-                              maxLines: 2,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ],
-                        ),
-                      ),
-                      Flexible(
-                        flex: 3,
-                        child: Align(
-                          alignment: Alignment.center,
-                          child: Text(
-                            '\$$precio',
-                            style: TextStyle(
-                              color: primaryColor,
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.bold
-                            ),
+                              Text(
+                                'Con pepinillos, salsa de tomate, vegetales, etc.',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 10.0
+                                ),
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ],
                           ),
                         ),
-                      )
-                    ],
+                        Flexible(
+                          flex: 3,
+                          child: Align(
+                            alignment: Alignment.center,
+                            child: Text(
+                              '\$$precio',
+                              style: TextStyle(
+                                color: primaryColor,
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
