9623ba4b627a0ace5edc65b2411c6ba8c89193eb
jirenuki-69 <mfuentesmodelo@gmail.com>
2020-08-11 17:39:15 -0500

    Cards

--- artemisfood/lib/src/pages/CartPage/cartPage.dart
+++ artemisfood/lib/src/pages/CartPage/cartPage.dart
@@ -1,9 +1,11 @@
 import 'package:artemisfood/src/components/RoundedButton.dart';
+import 'package:artemisfood/src/components/custom_bottom_body.dart';
 import 'package:artemisfood/src/pages/CartPage/components/cartCards.dart';
 import 'package:artemisfood/src/pages/HomePage/components/custom_app_bar.dart';
+import 'package:artemisfood/src/providers/app_bloc.dart';
 import 'package:artemisfood/src/static/const.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:provider/provider.dart';
 
 class CartPage extends StatefulWidget {
   @override
@@ -11,16 +13,7 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
-  List<DropdownMenuItem<String>> _metodoPago = [
-    DropdownMenuItem(
-      child: Text('Efectivo'),
-      value: 'efectivo',
-    ),
-    DropdownMenuItem(
-      child: Text('Tarjeta'),
-      value: 'tarjeta',
-    )
-  ];
+  
 
   String opcionSeleccionada;
 
@@ -30,26 +23,54 @@ class _CartPageState extends State<CartPage> {
     height: iconSize,
   );
 
-  void _seleccionarIcono(valor) {
+  void _seleccionarIcono(valor, BuildContext context) {
+    final appBloc = Provider.of<AppBloc>(context, listen: false);
+    final Color _color = appBloc.isDarkMode ? Colors.white : Colors.black;
+
     if (valor == 'tarjeta') {
       iconoSelect = Image(
         image: AssetImage('$iconPath/credit_card.png'),
         width: iconSize,
         height: iconSize,
+        color: _color,
       );
     } else {
       iconoSelect = Image(
         image: AssetImage('$iconPath/money.png'),
         width: iconSize,
         height: iconSize,
+        color: _color,
       );
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    final appBloc = Provider.of<AppBloc>(context, listen: false);
+    final Color _color = appBloc.isDarkMode ? Colors.white : Colors.black;
+    final Color _colorText = appBloc.isDarkMode ? Colors.white.withOpacity(0.9) : Colors.grey[600];
+    List<DropdownMenuItem<String>> _metodoPago = [
+      DropdownMenuItem(
+        child: Text('Efectivo', style: TextStyle(color: Colors.white),),
+        value: 'efectivo',
+      ),
+      DropdownMenuItem(
+        child: Text('Tarjeta', style: TextStyle(color: Colors.white),),
+        value: 'tarjeta',
+      )
+    ];
+
     return Scaffold(
+      backgroundColor: appBloc.isDarkMode ? backgroundHomeDark : backgroundHome,
       bottomSheet: Container(
+        decoration: BoxDecoration(
+          color: appBloc.isDarkMode ? backgroundHomeDark : Colors.white,
+          border: Border(
+            top: BorderSide(
+              color: Colors.transparent
+            ),
+          ),
+        ),
         //constraints: BoxConstraints(maxHeight:200, minHeight: 200,  ),
         height: 220,
         alignment: Alignment.bottomCenter,
@@ -66,18 +87,19 @@ class _CartPageState extends State<CartPage> {
                   image: AssetImage('$iconPath/wallet.png'),
                   width: iconSize,
                   height: iconSize,
+                  color: _color,
                 ),
                 Text('Cantidad a Pagar',
                     style: TextStyle(
                         fontSize: 20,
-                        color: Colors.grey[600],
+                        color: _colorText,
                         fontFamily: 'Avenir')),
                 SizedBox(
                   width: 60,
                 ),
                 Text('\$100',
                     style: TextStyle(
-                        fontSize: 20, color: Colors.grey[600], fontFamily: '')),
+                        fontSize: 20, color: primaryColor, fontFamily: '')),
               ],
             ),
             SizedBox(
@@ -90,20 +112,22 @@ class _CartPageState extends State<CartPage> {
                 Text('Método de Pago',
                     style: TextStyle(
                         fontSize: 20,
-                        color: Colors.grey[600],
+                        color: _colorText,
                         fontFamily: 'Avenir')),
                 SizedBox(
                   width: 25,
                 ),
                 DropdownButton(
-                    items: _metodoPago,
-                    value: opcionSeleccionada,
-                    onChanged: (opcion) {
-                      setState(() {
-                        opcionSeleccionada = opcion;
-                        _seleccionarIcono(opcion);
-                      });
-                    })
+                  dropdownColor: primaryColor,
+                  items: _metodoPago,
+                  value: opcionSeleccionada,
+                  onChanged: (opcion) {
+                    setState(() {
+                      opcionSeleccionada = opcion;
+                      _seleccionarIcono(opcion, context);
+                    });
+                  }
+                )
               ],
             ),
             RoundedButton(
@@ -119,42 +143,41 @@ class _CartPageState extends State<CartPage> {
       body: CustomScrollView(
         slivers: <Widget>[
           CustomAppBar(
-            bottomBody: Padding(
-              padding: const EdgeInsets.only(),
-              child: Text(
-                'Carrito de Compras',
-                style: TextStyle(
-                    color: primaryColor,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 22.0),
-              ),
-            ),
+            bottomBody: CustomBottomBody(title: 'Carrito de Compras')
           ),
           SliverList(
               delegate: SliverChildListDelegate([
             CartCard(
               image: 'https://cdn.kiwilimon.com/recetaimagen/33550/38940.jpg',
-              title: 'Pizza Peperonni',
+              title: 'Pizza de Peperonni',
               description: 'Queso, Peperonni, harina, huevos y no sé que más',
               price: 100,
               quantity: 3,
               comment: true,
             ),
-            CartCard2(
+            CartCard(
               image: 'https://cdn.kiwilimon.com/recetaimagen/33550/38940.jpg',
-              title: 'Pizza Peperonni',
+              title: 'Pizza de Peperonni',
               description: 'Queso, Peperonni, harina, huevos y no sé que más',
               price: 100,
               quantity: 3,
               comment: true,
             ),
-            CartCard2(
+            CartCard(
               image: 'https://cdn.kiwilimon.com/recetaimagen/33550/38940.jpg',
-              title: 'Pizza Peperonni',
+              title: 'Pizza de Peperonni',
               description: 'Queso, Peperonni, harina, huevos y no sé que más',
               price: 100,
               quantity: 3,
-              comment: false,
+              comment: true,
+            ),
+            CartCard(
+              image: 'https://cdn.kiwilimon.com/recetaimagen/33550/38940.jpg',
+              title: 'Pizza de Peperonni',
+              description: 'Queso, Peperonni, harina, huevos y no sé que más',
+              price: 100,
+              quantity: 3,
+              comment: true,
             ),//CartCard//CartCards2(),
             SizedBox(
               height: 220,
--- artemisfood/lib/src/pages/CartPage/components/cartCards.dart
+++ artemisfood/lib/src/pages/CartPage/components/cartCards.dart
@@ -1,7 +1,9 @@
 //import 'package:artemisfood/src/static/const.dart';
+import 'package:artemisfood/src/providers/app_bloc.dart';
 import 'package:artemisfood/src/static/const.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:provider/provider.dart';
 
 
 class CartCard extends StatelessWidget {
@@ -23,78 +25,95 @@ class CartCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final appBloc = Provider.of<AppBloc>(context, listen: false);
+    final Color _color = appBloc.isDarkMode ? Colors.white : Colors.black;
+    final Color _colorSubtitle = appBloc.isDarkMode ? Colors.white.withOpacity(0.8) : Colors.grey[500];
+    
     return Slidable(
       child: Padding(
         padding: EdgeInsets.all(8),
         child: Container(
           height: 100,
           decoration: BoxDecoration(
-              color: Colors.white,
+              color: Colors.transparent,
               borderRadius: BorderRadius.circular(20.0),
-              boxShadow: <BoxShadow>[
-                BoxShadow(
-                    color: Colors.black26,
-                    blurRadius: 10.0,
-                    spreadRadius: 1.0,
-                    offset: Offset(2.0, 10.0)),
-              ]),
+              // boxShadow: <BoxShadow>[
+              //   BoxShadow(
+              //       color: Colors.black26,
+              //       blurRadius: 10.0,
+              //       spreadRadius: 1.0,
+              //       offset: Offset(2.0, 10.0)),
+              //   ]
+              ),
           child: Row(
             children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Image(image: NetworkImage(image), width: 150, height: 100,)
-              ),
-              Container(   
-                padding: EdgeInsets.all(10),
-                width: 160,
-                child: Column(                  
-                  children: [
-                  Container(
-                    alignment: Alignment.centerLeft,
-                    child: Text(title, style: TextStyle(
-                    fontFamily: 'Avenir',
-                    fontSize: 18                   
-                    ),), 
+              Expanded(
+                flex: 3,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image(
+                      image: NetworkImage(image,),
+                    )
                   ),
-                  Text(description, softWrap: true,
-                  style: TextStyle(
-                    fontFamily: 'Avenir',
-                    fontSize: 16,
-                    color: Colors.grey[500]
-                  ),),     
-                  ],
                 ),
               ),
-              Container(              
-                //padding: EdgeInsets.all(5),
+              Flexible(
+                flex: 4,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(   
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,               
+                    children: [
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      child: Text(title, maxLines: 1, overflow: TextOverflow.ellipsis, style: TextStyle(
+                      fontFamily: 'Avenir',
+                      fontSize: 16,
+                      color: _color                   
+                      ),), 
+                    ),
+                    Text(description, softWrap: true, maxLines: 2, overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      fontFamily: 'Avenir',
+                      fontSize: 11,
+                      color: _colorSubtitle
+                    ),),     
+                    ],
+                  ),
+                ),
+              ),
+              Flexible(
+                flex: 2,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     SizedBox(height: 0),                    
-                    Container(
-                      //padding: EdgeInsets.all(5),
-                      //alignment: Alignment.center,
+                    Padding(
+                      padding: const EdgeInsets.only(right : 8.0),
                       child: Text('\$$price MXN', 
                         style: TextStyle(
                           fontFamily: 'Avenir',
-                          color: Colors.grey[600],
-                          fontSize: 16
+                          color: appBloc.isDarkMode ? Colors.white : primaryColor,
+                          fontSize: 14,
                         )
                       ),
                     ),
                     Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                        Container(                         
-                         padding: EdgeInsets.all(5),
-                         child: getComment(),
+                        //  padding: EdgeInsets.all(5),
+                         child: getComment(context),
                        ),
                        Container(                         
                          padding: EdgeInsets.all(8),
                          child: Text('X $quantity', 
                           style: TextStyle(
                             fontFamily: 'Avenir',
-                            color: Colors.grey[600],                            
+                            color: appBloc.isDarkMode ? Colors.white : Colors.grey[600],                            
                           ),
                          )
                        ) 
@@ -110,18 +129,23 @@ class CartCard extends StatelessWidget {
       actionPane: SlidableBehindActionPane(),
       secondaryActions: [
         Container(
+          padding: EdgeInsets.all(10.0),
           height: 100,
           child: ClipRRect(
             borderRadius: BorderRadius.circular(20),
             child: IconSlideAction(
+              foregroundColor: appBloc.isDarkMode ? Colors.white : Colors.red,
               caption: 'Borrar',
-              iconWidget: Image(
-                image: AssetImage('$iconPath/delete.png'),
-                color: Colors.white,
-                width: iconSize,
-                height: iconSize,
+              iconWidget: Padding(
+                padding: const EdgeInsets.only(bottom: 5.0),
+                child: Image(
+                  image: AssetImage('$iconPath/delete.png'),
+                  color: appBloc.isDarkMode ? Colors.white : Colors.red,
+                  width: iconSize,
+                  height: iconSize,
+                ),
               ),
-              color: Colors.red,
+              color: Colors.transparent,
             ),
           ),
         )
@@ -129,9 +153,10 @@ class CartCard extends StatelessWidget {
     );
   }
 
-  getComment() {
+  Widget getComment(BuildContext context) {
+    final appBloc = Provider.of<AppBloc>(context, listen: false);
     if (comment){
-      return Icon(Icons.comment, color: Colors.grey[600],);
+      return Icon(Icons.comment, color: appBloc.isDarkMode ? Colors.white : Colors.grey[600],);
     }
     else{
       return null;
@@ -251,19 +276,22 @@ class CartCard2 extends StatelessWidget {
       ),
       actionPane: SlidableBehindActionPane(),
       secondaryActions: [
-        Container(
-          height: 100,
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: IconSlideAction(
-              caption: 'Borrar',
-              iconWidget: Image(
-                image: AssetImage('$iconPath/delete.png'),
-                color: Colors.white,
-                width: iconSize,
-                height: iconSize,
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            height: 100,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: IconSlideAction(
+                caption: 'Borrar',
+                iconWidget: Image(
+                  image: AssetImage('$iconPath/delete.png'),
+                  color: Colors.white,
+                  width: iconSize,
+                  height: iconSize,
+                ),
+                color: Colors.red,
               ),
-              color: Colors.red,
             ),
           ),
         )
